cc621f7370ff66e031fd56c726b3fd4d10a51017
FLL128 <jonas@leuthold.me>
2023-12-13 11:16:09 +0100

    Merged both parts because they were so similar

--- Day13/main.py
+++ Day13/main.py
@@ -1,49 +1,31 @@
 file = open("input.txt","r")
 patterns = file.read().split("\n\n")
 
-# Part 1
-total = 0
-for pattern in patterns:
-    lines = pattern.split("\n")
-    # Horizontal lines
-    for i in range(1,len(lines)):
-        works = True
-        for j in range(1,min(len(lines)-i+1,i+1)):
-            if lines[i-j]!=lines[i+j-1]:
-                works = False
-        if works:
-            total += 100*(i)
-    # Vertical lines
-    for i in range(len(lines[0])):
-        works = True
-        for j in range(1,min(len(lines[0])-i+1,i+1)):
-            for k in range(len(lines)):
-                if lines[k][i-j]!=lines[k][i+j-1]:
-                    works = False
-        if works:
-            total += (i)
-print(total)
-
-# Part 2
-total = 0
+total1 = 0
+total2 = 0
 for pattern in patterns:
     lines = pattern.split("\n")
     # Horizontal lines
     for i in range(1,len(lines)):
         cnt = 0
-        for j in range(1,min(len(lines)-i+1,i+1)):
+        for j in range(1,min(len(lines)-i,i)+1):
             for k in range(len(lines[0])):
                 if lines[i-j][k]!=lines[i+j-1][k]:
                     cnt += 1
-        if cnt == 1:
-            total += 100*(i)
+        if cnt == 0:
+            total1 += 100*i
+        elif cnt == 1:
+            total2 += 100*i
     # Vertical lines
     for i in range(len(lines[0])):
         cnt = 0
-        for j in range(1,min(len(lines[0])-i+1,i+1)):
+        for j in range(1,min(len(lines[0])-i,i)+1):
             for k in range(len(lines)):
                 if lines[k][i-j]!=lines[k][i+j-1]:
                     cnt += 1
-        if cnt == 1:
-            total += (i)
-print(total)
+        if cnt == 0:
+            total1 += i
+        elif cnt == 1:
+            total2 += i
+print(total1)
+print(total2)
