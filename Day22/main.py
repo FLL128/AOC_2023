e1d3be696ed58917aac1f5424a9260f1a415ef9c
FLL128 <jonas@leuthold.me>
2023-12-26 15:01:11 +0100

    Improved code

--- Day22/main.py
+++ Day22/main.py
@@ -6,6 +6,8 @@ for line in lines:
     start = [int(x) for x in line.split("~")[0].split(",")]
     end = [int(x) for x in line.split("~")[1].split(",")]
     bricks.append((start,end))
+bricks.sort(key=lambda x: x[0][2])
+
 grid = []
 for i in range(10):
     plane = []
@@ -16,54 +18,72 @@ for i in range(10):
         plane.append(line)
     grid.append(plane)
 
+def place_brick(brick):
+    start,end = brick
+    for i in range(start[0],end[0]+1): 
+        for j in range(start[1],end[1]+1): 
+            for k in range(start[2],end[2]+1):
+                grid[i][j][k] = True
+                
+def remove_brick(brick):
+    start,end = brick
+    for i in range(start[0],end[0]+1): 
+        for j in range(start[1],end[1]+1): 
+            for k in range(start[2],end[2]+1):
+                grid[i][j][k] = False
+
 def place_bricks(bricks):
     for i in range(10):
         for j in range(10):
             for k in range(500):
                 grid[i][j][k] = False
     for brick in bricks:
-        start,end = brick
-        for i in range(start[0],end[0]+1): 
-            for j in range(start[1],end[1]+1): 
-                for k in range(start[2],end[2]+1):
-                    grid[i][j][k] = True
-                    
-place_bricks(bricks)
-
-for x in range(100):
-    for brick in bricks:
-        empty = True
-        start,end = brick
+        start,end = brick  
+        empty = True      
         while empty and start[2]>1:
             for i in range(start[0],end[0]+1): 
                     for j in range(start[1],end[1]+1):
                             if grid[i][j][start[2]-1]:
                                 empty = False
-            if empty and start[2]>1:
-                for i in range(start[0],end[0]+1): 
-                    for j in range(start[1],end[1]+1): 
-                        grid[i][j][start[2]-1] = True
-                        grid[i][j][end[2]] = False
+            if empty:
                 start[2] -= 1
                 end[2] -= 1
                 brick = (start,end)
-
-total = 0
-bricks_copy = bricks.copy()
-
-for test_brick in bricks_copy:
-    bricks.remove(test_brick)
-    place_bricks(bricks)
-    safe = True
-    for brick in bricks:
-        start,end = brick
-        empty = True
+        place_brick(brick)    
+                          
+place_bricks(bricks)
+                
+for brick in bricks:
+    empty = True
+    start,end = brick
+    while empty and start[2]>1:
         for i in range(start[0],end[0]+1): 
                 for j in range(start[1],end[1]+1):
                         if grid[i][j][start[2]-1]:
                             empty = False
+        if empty:
+            for i in range(start[0],end[0]+1): 
+                for j in range(start[1],end[1]+1): 
+                    grid[i][j][start[2]-1] = True
+                    grid[i][j][end[2]] = False
+            start[2] -= 1
+            end[2] -= 1
+            brick = (start,end)
+
+total = 0
+for i in range(len(bricks)):
+    remove_brick(bricks[i])
+    safe = True
+    for j in range(i+1,len(bricks)):
+        start,end = bricks[j]
+        empty = True
+        for k in range(start[0],end[0]+1): 
+                for l in range(start[1],end[1]+1):
+                        if grid[k][l][start[2]-1]:
+                            empty = False
         if empty and start[2]>1:
-            safe = False             
+            safe = False
+            break             
     if safe: total+=1
-    bricks.append(test_brick)
+    place_brick(bricks[i])
 print(total)
